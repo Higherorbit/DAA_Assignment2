93b8b448c44344138ae03580f3c965daee7849ad
Higherorbit <87420218+Higherorbit@users.noreply.github.com>
2023-04-20 12:42:55 +0530

    Thoda sa modified

--- dp.cpp
+++ dp.cpp
@@ -13,6 +13,7 @@ using namespace __gnu_pbds;
 #define prnt(x) cerr<<#x<<'{';for(auto T:x)cerr<<T<<' ';cerr<<"}\n";
 #define prntp(x) cerr<<#x<<'{';for(auto T:x)cerr<<T.first<<'-'<<T.second<<' ';cerr<<"}\n";
 const int mod = 1000000007;
+#define INF numeric_limits<double>::infinity()
 const int MAX=10000;
 #define FAST cin.tie(0)->sync_with_stdio(0);
 
@@ -21,7 +22,7 @@ typedef tree <int, null_type, less<int>, rb_tree_tag,
         pbds;
 
 struct point{
-	int x,y;
+	double x,y;
 	bool operator<(const point& cur) const{
 		return x<cur.x;
 	}
@@ -30,13 +31,61 @@ struct point{
 int32_t main(){
 	in(n)    ;
 	in(c);
-	for(int i=0;i<n;i++){
-		in(x);
-		in(y);
+	for(int i=1;i<=n;i++){
+		double x,y;
+		cin>>x>>y;
 		points[i].x=x;
 		points[i].y=y;
 	}
-	for(auto i:points){
-		cout<<i.x<<" "<<i.y<<endl;
+	std::vector<double> sumX,sumY,sumXY,sumXX;
+	sort(points+1,points+n+1);
+	sumX[0]=0;
+	sumY[0]=0;
+	sumXY[0]=0;
+	sumXX[0]=0;
+	for(int i=1;i<=n;i++){
+		sumX[i]=sumX[i-1]+points[i].x;
+		sumY[i]=sumY[i-1]+points[i].y;
+		sumXY[i]=sumXY[i-1]+points[i].x*points[i].y;
+		sumXX[i]=sumXX[i-1]+points[i].x*points[i].x;
 	}
+	double e[MAX+1][MAX+1];
+	memset(e,0.0,sizeof(e));
+	for(int j=1;j<=n;j++){
+		for(int i=1;i<=j;i++){
+			// calculate the slope and intercept for the line
+			// connecting pi,.. pj
+			double curSumX=sumX[j]-sumX[i-1];
+			double curSumY=sumY[j]-sumY[i-1];
+			double curSumXY=sumXY[j]-sumXY[i-1];
+			double curSumXX=sumXX[j]-sumXX[i-1];
+			double a,b;
+			double numSlope=(j-i+1)*(curSumXY)-curSumX*curSumY;
+			if(numSlope==0){
+				a=0.0;
+			}
+			else{
+				if((j-i+1)*(curSumXX)==curSumX*curSumX){
+					a=INF;
+				}
+				else{
+					a=numSlope/((j-i+1)*(curSumXX)-curSumX*curSumX);
+				}
+			}
+			b=curSumY-(j-i+1)*curSumX/(j-i+1);
+			// now we need to calculate the error
+			for(int k=i;k<=j;k++){
+				double err=points[k].y-(a*points[k].x+b);
+				e[i][j]+=err*err;
+			}
+		}
+	}
+	vector<double>dp(MAX+1,INT_MAX);
+	dp[0]=0;
+	for(int j=1;j<=n;j++){
+		for(int i=1;i<=j;i++){
+			dp[j]=min(dp[j],e[i][j]+c+dp[i-1]);
+		}
+	}
+	cout<<dp[n]<<endl;
 }
